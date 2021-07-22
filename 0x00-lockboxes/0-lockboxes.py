8e46fb67c3ad479c6d1a6c33cf3c2f9f186fead4
Brandixitor <med.salah.bourouba@gmail.com>
2021-07-22 12:53:02 +0100

    Fixed

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,21 +2,20 @@
 
 
 def canUnlockAll(boxes):
-    """ Function that unlocks all boxes """
-
-    checked = [False for v in range(len(boxes))]
-    checked[0] = True
+    """ Unlocks all boxes """
+    visited = [False for v in range(len(boxes))]
+    visited[0] = True
     stack = boxes[0]
 
-        while(len(stack)):
-            element = stack[-1]
-            stack.pop()
+    while(len(stack)):
+        element = stack[-1]
+        stack.pop()
         try:
             if not visited[element]:
-                checked[element] = True
+                visited[element] = True
             for node in boxes[element]:
-                if not checked[node]:
+                if not visited[node]:
                     stack.append(node)
         except:
             pass
-    return all(checked)
+    return all(visited)
