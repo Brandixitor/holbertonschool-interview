38318a076e1dcf1288edcee78dfe8a32d8aac1c6
Med Salah Bourouba <68739838+Brandixitor@users.noreply.github.com>
2021-09-02 22:09:31 +0100

    Add files via upload

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,38 +1,38 @@
 #!/usr/bin/python3
+"""
+Script that reads stdin line by line and computes metrics
+"""
 import sys
-import collections
-""" Script that reads stdin line by line and computes metrics. """
 
-i = 0
-size = 0
-key = ""
-metrics = collections.OrderedDict({})
-possible_code = ['200', '301', '400', '401', '403', '404', '405', '500']
+total_size = 0
+counter = 0
 
-
-def print_stuff():
-    """ Prints file isze + metrics """
-    print("File size: {}".format(str(size)))
-    for k in sorted(metrics):
-        print("{}: {}".format(str(k), str(metrics[k])))
-
+dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0, '405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
-        b = [str(x) for x in line.split(' ')]
-        if len(b) > 2:
-            key = b[-2]
-            if (key in metrics.keys()):
-                metrics[key] += 1
-            else:
-                if key in possible_code:
-                    metrics[key] = 1
-            size += int(b[-1])
-        i += 1
-        if (i == 10):
-            print_stuff()
-            i = 0
-except KeyboardInterrupt:
+        line_list = line.split(" ")
+        if len(line_list) > 2:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in dict_codes_counter.keys():
+                dict_codes_counter[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            print("File size: {:d}".format(total_size))
+            for k, v in sorted(dict_codes_counter.items()):
+                if v != 0:
+                    print("{}: {:d}".format(k, v))
+            counter = 0
+
+except Exception:
     pass
+
 finally:
-    print_stuff()
+    print("File size: {}".format(total_size))
+    for k, v in sorted(dict_codes_counter.items()):
+        if v != 0:
+            print("{}: {}".format(k, v))
--- 0x06-log_parsing/README.md
+++ 0x06-log_parsing/README.md
@@ -1,2 +1,1 @@
 # 0x06-log_parsing
-
