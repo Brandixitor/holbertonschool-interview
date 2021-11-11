d0ef88397626e52081f5d65c098d95a64cd4278a
Brandixitor <med.salah.bourouba@gmail.com>
2021-11-11 13:08:55 -0800

    Fix

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,16 +1,24 @@
 #!/usr/bin/node
-
-const r = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-
-r.get(url, (err, res, body) => {
-  if (err) console.log(err);
+/* using API to print the title film  */
+const request = require('request');
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+/* create a mapped dict for completed tasks */
+request.get(url, async (error, res) => {
+  if (error) console.log(error);
   else {
-    JSON.parse(body).characters.forEach(character => {
-      r.get(character, (err, res, body) => {
-        if (err) console.log(err);
-        else console.log(JSON.parse(body).name);
-      });
-    });
+    const mainErray = JSON.parse(res.body);
+    tryExecuteNext(mainErray.characters, 0);
   }
 });
+
+const tryExecuteNext = (mainErray, index) => {
+  request.get(mainErray[index], (error, res, body) => {
+    if (error) throw error;
+    const result = JSON.parse(body);
+    console.log(result.name);
+    if (index < mainErray.length - 1) {
+      tryExecuteNext(mainErray, index + 1);
+    }
+  });
+};
