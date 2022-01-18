bb3ad32be60fafec0c4f1ba7b490cef305d2e686
Med Salah Bourouba <68739838+Brandixitor@users.noreply.github.com>
2022-01-18 15:21:59 +0100

    Update 0-starwars_characters.js

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,24 +1,22 @@
 #!/usr/bin/node
-/* using API to print the title film  */
+
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
-/* create a mapped dict for completed tasks */
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get(url, async (error, res) => {
   if (error) console.log(error);
   else {
-    const mainErray = JSON.parse(res.body);
-    tryExecuteNext(mainErray.characters, 0);
+    const list = JSON.parse(res.body);
+    tryExecuteNext(list.characters, 0);
   }
 });
 
-const tryExecuteNext = (mainErray, index) => {
-  request.get(mainErray[index], (error, res, body) => {
+const tryExecuteNext = (list, index) => {
+  request.get(list[index], (error, res, body) => {
     if (error) throw error;
     const result = JSON.parse(body);
     console.log(result.name);
-    if (index < mainErray.length - 1) {
-      tryExecuteNext(mainErray, index + 1);
+    if (index < list.length - 1) {
+      tryExecuteNext(list, index + 1);
     }
   });
 };
