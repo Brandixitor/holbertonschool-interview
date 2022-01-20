8621ff8850b5ad9362521e39ba69ee2474fc4aff
Brandixitor <med.salah.bourouba@gmail.com>
2022-01-20 15:39:19 +0100

    Update

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -6,17 +6,17 @@ request.get(url, async (error, res) => {
   if (error) console.log(error);
   else {
     const list = JSON.parse(res.body);
-    tryExecuteNext(list.characters, 0);
+    Execute(list.characters, 0);
   }
 });
 
-const tryExecuteNext = (list, index) => {
+const Execute = (list, index) => {
   request.get(list[index], (error, res, body) => {
     if (error) throw error;
     const result = JSON.parse(body);
     console.log(result.name);
     if (index < list.length - 1) {
-      tryExecuteNext(list, index + 1);
+      Execute(list, index + 1);
     }
   });
 };
